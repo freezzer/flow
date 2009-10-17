4e63d023449fc86eb1029fc217631266914c8f8e
andreas.marochow@gmail.com <andreas.marochow@gmail.com@b3cf1fae-f098-11dd-ac5b-dfceb64ead24>
2009-10-17 21:44:11 +0000

    Client Server

---
+++ trunk/bin/server.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+HOME=..
+LIB=$HOME/lib
+ 
+CP=$HOME/classes
+CP=$CP:$LIB/mysql-connector-java-5.1.0-bin.jar
+CP=$CP:$LIB/gna-jorm-runtime-05.11.jar
+CP=$CP:$LIB/jetty-6.1.15.rc3.jar
+CP=$CP:$LIB/jetty-util-6.1.15.rc3.jar
+CP=$CP:$LIB/hessian-3.2.1.jar
+CP=$CP:$LIB/servlet-api.jar 
+CP=$CP:$LIB/click-2.0.1-incubating.jar
+CP=$CP:$LIB/mail.jar
+CP=$CP:$LIB/activation.jar
+CP=$CP:$LIB/stringtree-json-2.0.9.jar
+echo $CP
+
+java -classpath $CP de.ama.Starter > $HOME/log/server.log 2>&1 &
+
+
---
+++ trunk/bin/setenv.bat
@@ -0,0 +1,21 @@
+set JAVA=D:\Java\jdk1.5.0_03\bin\java
+set HOME=D:\dev\flow\server
+set LIB=%HOME%\lib
+set CATALOG=flow
+
+set CP=%HOME%\classes
+set CP=%CP%;%HOME%\resources
+set CP=%CP%;%LIB%\mysql-connector-java-5.1.0-bin.jar
+set CP=%CP%;%LIB%\gna-jorm-runtime-05.11.jar
+set CP=%CP%;%LIB%\jetty-6.1.15.rc3.jar
+set CP=%CP%;%LIB%\jetty-util-6.1.15.rc3.jar
+set CP=%CP%;%LIB%\hessian-3.2.1.jar
+set CP=%CP%;%LIB%\servlet-api.jar
+set CP=%CP%;%LIB%\click-2.0.1-incubating.jar
+set CP=%CP%;%LIB%\mail.jar
+set CP=%CP%;%LIB%\activation.jar
+set CP=%CP%;%LIB%\stringtree-json-2.0.9.jar
+
+
+
+
